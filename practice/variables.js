56db3e9ada2c46986c7ef01b8a8bf29740e5c39e
Robert Hardy <rhardy@dcsdk12.org>
2022-11-01 16:22:24 -0600

    More changes to variables

--- practice/variables.js
+++ practice/variables.js
@@ -6,5 +6,20 @@ let x = 500; // set x to have the value 500
 let y = 276;
 
 let sum = x + y;
-
 let pi = 3.14;
+
+console.log("x is ", x);
+
+let name = "Robert Hardy";
+
+let whatWillHappen = name + x;
+
+console.log(whatWillHappen, sum);
+
+let crazMAth; // = (100 * x) / (sum + whatWillHappen) - y;
+
+console.log(crazMAth);
+
+let isGameOver = false;
+
+console.log(isGameOver, typeof isGameOver);
