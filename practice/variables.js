c43baa77e46e85f0ae3405cb744719980450255d
Robert Hardy <rhardy1@dcsdk12.org>
2022-11-01 15:48:54 -0600

    Practicing with variables

---
+++ practice/variables.js
@@ -0,0 +1,10 @@
+console.log("Hello world!");
+
+// then I can write anything I want
+// and it wont get errors because it is a comment
+let x = 500; // set x to have the value 500
+let y = 276;
+
+let sum = x + y;
+
+let pi = 3.14;
