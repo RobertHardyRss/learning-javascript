1a85d1f18892725bbc25c76eed3d1ef5fbd2aa92
Robert Hardy <rhardy1@dcsdk12.org>
2022-11-08 16:34:58 -0700

    Adding draw square function.

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,4 +1,22 @@
 let canvas = document.getElementById("canvas-1");
+/** @type {CanvasRenderingContext2D} */
 let context = canvas.getContext("2d");
 
-context.fillRect(50, 50, 25, 25);
+let squares = [];
+
+drawSquare(0, 0, "red");
+drawSquare(25, 0, "blue");
+drawSquare(50, 0, "green");
+drawSquare(75, 0, "purple");
+
+function drawSquare(x, y, color, size = 25) {
+	let square = new Path2D();
+	square.rect(x, y, size, size);
+
+	squares.push(square);
+
+	context.fillStyle = color;
+	context.fillRect(x, y, size, size);
+}
+
+console.log(squares);
