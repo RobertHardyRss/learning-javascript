23e7f0631dea2342d74c9bd9a6d9cec6632ebeee
Robert Hardy <rhardy1@dcsdk12.org>
2022-11-10 17:25:36 -0700

    Add color animations.

--- practice/canvas.js
+++ practice/canvas.js
@@ -14,18 +14,6 @@ let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 // row = row + 1
 // row +=  1
 // row++
-for (let row = 0; row < gridSize; row++) {
-	for (let col = 0; col < gridSize; col++) {
-		let colorIndex = Math.floor(Math.random() * colors.length);
-		let color = colors[colorIndex];
-		drawSquare(col * size, row * size, color, size);
-	}
-}
-
-// drawSquare(0, 0, "red");
-// drawSquare(25, 0, "blue");
-// drawSquare(50, 0, "green");
-// drawSquare(75, 0, "purple");
 
 function drawSquare(x, y, color, size = 25) {
 	let square = new Path2D();
@@ -38,3 +26,30 @@ function drawSquare(x, y, color, size = 25) {
 }
 
 console.log(squares);
+
+let currentTime = 0;
+const refreshRate = 500;
+let lastRefresh = 0;
+
+function drawLoop(timestamp) {
+	let elapsedTime = timestamp - currentTime;
+	currentTime = timestamp;
+
+	lastRefresh = lastRefresh + elapsedTime;
+
+	if (lastRefresh >= refreshRate) {
+		lastRefresh = 0;
+
+		for (let row = 0; row < gridSize; row++) {
+			for (let col = 0; col < gridSize; col++) {
+				let colorIndex = Math.floor(Math.random() * colors.length);
+				let color = colors[colorIndex];
+				drawSquare(col * size, row * size, color, size);
+			}
+		}
+	}
+
+	requestAnimationFrame(drawLoop);
+}
+
+requestAnimationFrame(drawLoop);
