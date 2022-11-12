51557863d938d6b8a837fb68067276a592ea265f
Robert Hardy <rhardy1@dcsdk12.org>
2022-11-11 17:14:26 -0700

    Create ClickBox class.
    Create ClickBox class.

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,19 +1,52 @@
+//@ts-check
+/** @type {HTMLCanvasElement} */ //@ts-ignore
 let canvas = document.getElementById("canvas-1");
 canvas.width = 100;
 canvas.height = 100;
 
-/** @type {CanvasRenderingContext2D} */
+/** @type {CanvasRenderingContext2D} */ //@ts-ignore
 let context = canvas.getContext("2d");
+
+
+
+class ClickBox {
+	constructor(x, y, size, colors) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+
+		this.isClicked = false;
+		this.refreshRate = 500;
+		this.lastRefresh = 0;
+		this.colors = colors;
+		this.color = "red";
+
+		this.setColor();
+	}
+
+	setColor() {
+		let colorIndex = Math.floor(Math.random() * this.colors.length);
+		this.color = this.colors[colorIndex];
+	}
+
+	update(timeElapsed) {
+		this.lastRefresh += timeElapsed;
+
+		if(this.lastRefresh < this.refreshRate) return;
+
+
+		if(this.lastRefresh >= this.refreshRate) {
+			this.setColor();
+		}
+	}
+
+	draw() {}
+}
 
 let squares = [];
 let gridSize = 4;
 let size = canvas.width / gridSize;
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-
-// All of these incrementors do the same
-// row = row + 1
-// row +=  1
-// row++
 
 function drawSquare(x, y, color, size = 25) {
 	let square = new Path2D();
@@ -40,6 +73,10 @@ function drawLoop(timestamp) {
 	if (lastRefresh >= refreshRate) {
 		lastRefresh = 0;
 
+		// All of these incrementors do the same
+		// row = row + 1
+		// row +=  1
+		// row++
 		for (let row = 0; row < gridSize; row++) {
 			for (let col = 0; col < gridSize; col++) {
 				let colorIndex = Math.floor(Math.random() * colors.length);
